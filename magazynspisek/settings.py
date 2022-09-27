40602637a8cb85c1b3c09f48b0609d1d3ffa1ab6
Kajetan Maślak <kajtek.maslak@icloud.com>
2022-09-27 11:31:30 +0200

    Connected with PostgreSQL database

--- magazynspisek/settings.py
+++ magazynspisek/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'magazynspisek.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'magazynspisek',
+        'USER': 'postgres',
+        'PASSWORD': 'admin1',
+        'HOST': '',
+        'PORT': '5432'
     }
 }
 
